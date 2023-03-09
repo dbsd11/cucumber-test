036625534cc23918d1c60a6dfcb1f4812b8c257f
diaobisong <bdiao@addx.ai>
2023-03-09 12:05:20 +0800

    add h2 database

--- src/main/java/group/bison/cucumber/AutoQAApplication.java
+++ src/main/java/group/bison/cucumber/AutoQAApplication.java
@@ -4,15 +4,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableWebFlux
+@EnableCaching
 @MapperScan("group.bison.cucumber.dao")
 public class AutoQAApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             SpringApplication.run(AutoQAApplication.class, args);
         } catch (Throwable e) {
