c89ef30f7bce570bab45bc08639b01a55fb4b35c
dbsd11 <bitdbsd11@163.com>
2023-04-12 00:28:31 +0800

    fix code

--- src/main/java/group/bison/cucumber/AutoQAApplication.java
+++ src/main/java/group/bison/cucumber/AutoQAApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @EnableWebFlux
 @EnableCaching
-@MapperScan("group.bison.cucumber.rest.dao")
+@MapperScan("group.bison.cucumber.rest.repository")
 public class AutoQAApplication {
 
     public static void main(String[] args) throws Exception {
