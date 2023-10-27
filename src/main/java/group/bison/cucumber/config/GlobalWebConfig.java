af324076d468af1cbb443a9944a4292398be2eac
dbsd11 <bitdbsd11@163.com>
2023-10-27 22:37:50 +0800

    fix code

--- src/main/java/group/bison/cucumber/config/GlobalWebConfig.java
+++ src/main/java/group/bison/cucumber/config/GlobalWebConfig.java
@@ -3,19 +3,26 @@ package group.bison.cucumber.config;
 import group.bison.netty.modules.springcontext.RootSpringContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
 public class GlobalWebConfig implements ApplicationContextAware, InitializingBean {
 
     private ApplicationContext applicationContext;
+
+    @Autowired(required = false)
+    private List<HandlerMapping> handlerMappingList;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
@@ -28,6 +35,10 @@ public class GlobalWebConfig implements ApplicationContextAware, InitializingBea
             return;
         }
 
+        if(CollectionUtils.isEmpty(handlerMappingList)) {
+            return;
+        }
+
         try {
             ApplicationContext rootApplicationContext = RootSpringContextHolder.get();
             DefaultListableBeanFactory rootBeanFactory = (DefaultListableBeanFactory)rootApplicationContext.getAutowireCapableBeanFactory();
@@ -36,7 +47,10 @@ public class GlobalWebConfig implements ApplicationContextAware, InitializingBea
             String[] handlerMappingBeanNames = defaultListableBeanFactory.getBeanNamesForType(HandlerMapping.class);
 
             for(String handlerMappingBeanName : handlerMappingBeanNames) {
-                HandlerMapping handlerMapping = (HandlerMapping)defaultListableBeanFactory.getBean(handlerMappingBeanName);
+                Object handlerMapping = defaultListableBeanFactory.getBean(handlerMappingBeanName);
+                if(!(handlerMapping instanceof HandlerMapping)) {
+                    break;
+                }
                 String register2RootHandlerMappingBeanName = String.join("", "cucumber_", handlerMappingBeanName);
                 if(rootBeanFactory.containsBean(register2RootHandlerMappingBeanName)) {
                     rootBeanFactory.destroySingleton(register2RootHandlerMappingBeanName);
